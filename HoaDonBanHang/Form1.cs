3df4abd05443c693bd366e96b2ced79f67782e78
Manh Tuan <buimanhtuan2k4@gmail.com>
2025-08-19 14:32:13 +0700

    add bte

---
+++ HoaDonBanHang/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HoaDonBanHang
+{
+    public partial class Form1 : Form
+    {
+        int thanhTien = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            int soLuong = int.Parse(txtSoLuong.Text);
+            int donGia = int.Parse(txtDonGia.Text);
+            thanhTien = soLuong * donGia;
+            txtThanhTien.Text = thanhTien.ToString();
+        }
+
+        private void btnThongKe_Click(object sender, EventArgs e)
+        {
+            double giamGia = 0;
+            txtTongChuaThue.Text = thanhTien.ToString();
+
+            double thue = (thanhTien * 10) / 100.0;
+            txtThue.Text = thue.ToString();
+
+            if (chkKHTT.Checked)
+            {
+                giamGia = (thanhTien * 5) / 100.0;
+            }
+
+            txtGiamGia.Text = giamGia.ToString();
+            txtTongTienTra.Text = (thanhTien + thue - giamGia).ToString();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult th = MessageBox.Show("Bạn có muốn thoát", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (th == DialogResult.OK)
+            {
+                this.Close();
+            }
+            else
+            {
+
+            }
+        }
+        private void btnTiep_Click(object sender, EventArgs e)
+        {
+            txtTenHang.Clear();
+            txtSoLuong.Clear();
+            txtDonGia.Clear();
+            txtThanhTien.Clear();
+            txtTenHang.Focus();
+        }
+
+        private void btnKhachHangMoi_Click(object sender, EventArgs e)
+        {
+            txtTongChuaThue.Clear();
+            txtThue.Clear();
+            txtGiamGia.Clear();
+            txtTongTienTra.Clear();
+        }
+    }
+}
---
+++ HoaDonBanHang/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HoaDonBanHang")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HoaDonBanHang")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e363f5b-d650-466f-8956-d3981cae4004")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
